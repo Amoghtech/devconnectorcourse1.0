2775c928e8ba01e484ee54eb8b71fe548a5b5aa9
Amoghtech <amoghnagar1111@gmail.com>
2021-01-01 19:33:46 +0530

    'Deploying project'

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const app = express();
-require("dotenv").config();
+const path = require('path');
+require('dotenv').config();
 const PORT = process.env.PORT || 4000;
 
 connectDB();
-app.get('/', (req, res) => {
-  res.send('API RUNNING');
-});
 
 // Init middleware
-app.use(express.json({extended:false}));/*as express has body-parser module builtin */
+app.use(
+  express.json({ extended: false })
+); /*as express has body-parser module builtin */
 
 // Define routes
 
@@ -19,4 +19,13 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/post', require('./routes/api/post'));
 
-app.listen(PORT, ()=> console.log(`Server started on port ${PORT}`));
+// Serve static essets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
